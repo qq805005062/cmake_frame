2f6905993ad3c60d9a881b886d8071d8d313f39a
zhaoxiaoxiao <zhao_xiaoxiao@foxmail.com>
2018-03-29 17:52:53 +0800

    add redis async test method and always core

--- src/redislibevent/RedisAsync.cpp
+++ src/redislibevent/RedisAsync.cpp
@@ -10,14 +10,14 @@ static volatile int64_t requestNum = 0;
 
 static void connectCallback(const redisAsyncContext *c, int status)
 {
-	PDEBUG("RedisOnConnection connectCallback success\n");
+	PDEBUG("RedisOnConnection connectCallback\n");
 	RedisAsync *pRedis = static_cast<RedisAsync *>(c->data);
 	pRedis->RedisOnConnection(c,status,true);
 }
 
 static void disconnectCallback(const redisAsyncContext *c, int status)
 {
-	PDEBUG("RedisOnConnection disconnectCallback success\n");
+	PDEBUG("RedisOnConnection disconnectCallback\n");
 	RedisAsync *pRedis = static_cast<RedisAsync *>(c->data);
 	pRedis->RedisOnConnection(c,status,false);
 }
@@ -28,7 +28,7 @@ static void respondCallback(redisAsyncContext *c, void *r, void *privdata) {
 	requestNum--;
 	if(reply == NULL)
 	{
-		PERROR("");
+		PERROR("respondCallback redisReply NULL,This suitation will never happen,big problem\n");
 		return;
 	}
 
@@ -36,6 +36,7 @@ static void respondCallback(redisAsyncContext *c, void *r, void *privdata) {
 	{
 		case REDIS_REPLY_STRING:
 			{
+				PDEBUG("respondCallback REDIS_REPLY_STRING type :: %s -- %d -- %lld -- %lu\n",reply->str,reply->len,reply->integer,reply->elements);
 				if(pRedisRequest->IsResultCallBack())
 				{
 					if(reply->str && reply->len && strcasecmp(reply->str, "OK") == 0)
@@ -69,6 +70,7 @@ static void respondCallback(redisAsyncContext *c, void *r, void *privdata) {
 			break;
 		case REDIS_REPLY_ARRAY:
 			{
+				PDEBUG("respondCallback REDIS_REPLY_ARRAY type :: %s -- %d -- %lld -- %lu\n",reply->str,reply->len,reply->integer,reply->elements);
 				if(pRedisRequest->IsResultCallBack())
 				{
 					int64_t ret = 0;
@@ -97,6 +99,7 @@ static void respondCallback(redisAsyncContext *c, void *r, void *privdata) {
 			break;
 		case REDIS_REPLY_INTEGER:
 			{
+				PDEBUG("respondCallback REDIS_REPLY_INTEGER type :: %s -- %d -- %lld -- %lu\n",reply->str,reply->len,reply->integer,reply->elements);
 				if(pRedisRequest->IsResultCallBack())
 				{
 					int64_t ret = reply->integer;
@@ -121,6 +124,7 @@ static void respondCallback(redisAsyncContext *c, void *r, void *privdata) {
 			break;
 		case REDIS_REPLY_STATUS:
 			{
+				PDEBUG("respondCallback REDIS_REPLY_STATUS type :: %s -- %d -- %lld -- %lu\n",reply->str,reply->len,reply->integer,reply->elements);
 				if(pRedisRequest->IsResultCallBack())
 				{
 					int64_t ret = 0;
@@ -145,6 +149,7 @@ static void respondCallback(redisAsyncContext *c, void *r, void *privdata) {
 			break;
 		case REDIS_REPLY_NIL:
 			{
+				PDEBUG("respondCallback REDIS_REPLY_NIL type :: %s -- %d -- %lld -- %lu\n",reply->str,reply->len,reply->integer,reply->elements);
 				if(pRedisRequest->IsResultCallBack())
 				{
 					int64_t ret = 0;
@@ -169,6 +174,7 @@ static void respondCallback(redisAsyncContext *c, void *r, void *privdata) {
 			break;
 		case REDIS_REPLY_ERROR:
 			{
+				PDEBUG("respondCallback REDIS_REPLY_ERROR type :: %s -- %d -- %lld -- %lu\n",reply->str,reply->len,reply->integer,reply->elements);
 				if(pRedisRequest->IsResultCallBack())
 				{
 					int64_t ret = -1;
@@ -210,10 +216,12 @@ RedisAsync::RedisAsync(struct event_base* base)
 	,port_(0)
 	,conns_()
 {
+	PDEBUG("RedisAsync\n");
 }
 
 RedisAsync::~RedisAsync()
 {
+	PDEBUG("RedisAsync exit\n");
 }
 
 int RedisAsync::RedisConnect(const std::string host,int port,int num)
@@ -232,23 +240,24 @@ int RedisAsync::RedisConnect(const std::string host,int port,int num)
 		return -2;
 	}
 
+	PDEBUG("RedisConnect :: host :: %s,port :: %d,connnum :: %d\n",host.c_str(),port,num);
 	host_ = host;
 	port_ = port;
+	conInitNum = num;
 	for(int i = 0; i < num; i++)
 	{
-		PDEBUG("redisAsyncConnect :: %s %d\n",host.c_str(), port);
 		redisAsyncContext *c = redisAsyncConnect(host.c_str(), port);
 		if (c->err)
 		{
-			PERROR("RedisConnect redisAsyncConnect :: %s",c->errstr);
+			PERROR("RedisConnect redisAsyncConnect :: %s -- %d\n",c->errstr,i);
 			return -2;
 		}
 		redisLibeventAttach(c,libevent);
 		c->data = this;
 		redisAsyncSetConnectCallback(c,connectCallback);
     	redisAsyncSetDisconnectCallback(c,disconnectCallback);
-		conInitNum++;
 		conns_.push_back(c);
+		PDEBUG("RedisConnect redisAsyncConnect :: %d\n",i);
 	}
 	return 0;
 }
@@ -261,7 +270,7 @@ int RedisAsync::ResetConnect(const std::string host,int port)
 	
 	RedisBlockClear();
 	ConstRedisAsyncConn ().swap(conns_);
-	
+	PDEBUG("RedisAsync :: ResetConnect :: clear before\n");
 	for(int i = 0; i < conInitNum; i++)
 	{
 		redisAsyncContext *c = redisAsyncConnect(host.c_str(), port);
@@ -274,6 +283,8 @@ int RedisAsync::ResetConnect(const std::string host,int port)
 		c->data = this;
 		redisAsyncSetConnectCallback(c,connectCallback);
     	redisAsyncSetDisconnectCallback(c,disconnectCallback);
+		conns_.push_back(c);
+		PDEBUG("RedisConnect redisAsyncConnect ResetConnect :: %d\n",i);
 	}
 	return 0;
 }
@@ -285,7 +296,7 @@ void RedisAsync::RedisOnConnection(const redisAsyncContext *c,int status,bool bC
 		if (status == REDIS_OK)
 		{
 			conSuccNum++;
-			PDEBUG("RedisOnConnection conn success\n");
+			PDEBUG("RedisOnConnection conn success num :: %d\n",conSuccNum);
 		}else{
 			PERROR("connectCallback Error: %s\n", c->errstr);
 		}
@@ -295,18 +306,27 @@ void RedisAsync::RedisOnConnection(const redisAsyncContext *c,int status,bool bC
 			common::MutexLockGuard lock(IterLock);
 			for(ConstRedisAsyncConnIter iter = conns_.begin(); iter != conns_.end(); iter++)
 			{
+				
 				if(*iter == c)
+				{
+					redisLibeventCleanup((*iter)->ev.data);
+					(*iter)->ev.data = NULL;
+					redisAsyncFree(*iter);
 					conns_.erase(iter);
+				}
 			}
 			conSuccNum--;
-			PDEBUG("RedisOnConnection disconn success\n");
+			PDEBUG("RedisOnConnection disconn now conn num :: %d -- %ld\n",conSuccNum,conns_.size());
 		}else{
 			PERROR("disconnectCallback Error: %s\n", c->errstr);
 		}
 	}
 
 	if(conSuccNum == conInitNum && asyStatus == 0)
+	{
+		PDEBUG("redis connection init num all success\n");
 		asyStatus = 1;
+	}
 }
 
 void RedisAsync::RedisLoop()
@@ -318,33 +338,32 @@ void RedisAsync::RedisLoop()
 void RedisAsync::RedisBlockClear()
 {
 	asyStatus = 0;
+	PDEBUG("RedisAsync :: RedisBlockClear :: %ld\n",requestNum);
 	while(requestNum > 0)
 		usleep(100);
-
-	for(int i = 0; i < conInitNum; i++)
+	PDEBUG("RedisAsync :: RedisBlockClear :: %ld\n",requestNum);
+	for(int i = 0; i < conSuccNum; i++)
 	{
-		redisAsyncDisconnect(conns_[lastIndex]);
+		redisAsyncDisconnect(conns_[i]);
 	}
-	
+	PDEBUG("RedisAsync :: RedisBlockClear redisAsyncDisconnect :: %d\n",conSuccNum);
 	for(int i = 0; i < conInitNum; i++)
 	{
 		redisLibeventCleanup(conns_[lastIndex]->ev.data);
 		conns_[lastIndex]->ev.data = NULL;
 		redisAsyncFree(conns_[lastIndex]);
 	}
+	PDEBUG("RedisAsync :: RedisBlockClear redisLibeventCleanup :: %d\n",conSuccNum);
 	return;
 }
 
 int RedisAsync::set(const std::string& key, const std::string& value, const CmdResultCallBack& retCb, void *priv)
 {
-	if(asyStatus == 0)
-	{
-		PERROR("redis async no ready now\n");
-		return -3;
-	}
+	while(asyStatus == 0)
+		usleep(100);
 	if(key.empty() || value.empty())
 	{
-		PERROR("");
+		PERROR("RedisAsync set paramter null\n");
 		return -1;
 	}
 	
@@ -362,6 +381,7 @@ int RedisAsync::set(const std::string& key, const std::string& value, const CmdR
 		PERROR("redisAsyncCommand error :: %d\n", ret);
 		return -1;
 	}
+	PDEBUG("RedisAsync set %s %s\n",key.c_str(), value.c_str());
 	requestNum++;
 	lastIndex++;
 	if(lastIndex >= conSuccNum)
@@ -377,11 +397,8 @@ int RedisAsync::get(const std::string& key, CmdStrValueCallBack strCb, void *pri
 
 int RedisAsync::hmset(const std::string& key, const HashMap& hashMap, const CmdResultCallBack& retCb, void *priv)
 {
-	if(asyStatus == 0)
-	{
-		PERROR("redis async no ready now\n");
-		return -3;
-	}
+	while(asyStatus == 0)
+		usleep(100);
 	if(key.empty() || hashMap.empty())
 	{
 		PERROR();
@@ -422,6 +439,7 @@ int RedisAsync::hmset(const std::string& key, const HashMap& hashMap, const CmdR
 		PERROR("redisAsyncCommandArgv error :: %d\n", ret);
 		return -1;
 	}
+	PDEBUG("RedisAsync hmset key %s\n",key.c_str());
 	requestNum++;
 	lastIndex++;
 	if(lastIndex >= conSuccNum)
--- src/redislibevent/redislibevent.cpp
+++ src/redislibevent/redislibevent.cpp
@@ -1,60 +1,90 @@
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <signal.h>
+
+#include <pthread.h>
+
+#include <common/Timestamp.h>
 
 #include "RedisAsync.h"
 
-#if 0
-void getCallback(redisAsyncContext *c, void *r, void *privdata) {
-    redisReply *reply = static_cast<redisReply *>(r);
-    if (reply == NULL) return;
-    printf("argv[%s]: %s\n", (char*)privdata, reply->str);
+static ASYNCREDIS::RedisAsync hha;
+static int respond = 0;
+static int64_t beginS = 0,endS = 0;
 
-    /* Disconnect after receiving the reply to GET */
-    redisAsyncDisconnect(c);
+#define TESTNUM		400
+
+void hMsetCallBack(int64_t ret, void *privdata, const std::string& err)
+{
+	respond++;
+	if(!err.empty())
+	{
+		PERROR("hMsetCallBack :: error :: %s\n",err.c_str());
+		return;
+	}
+	if(respond == TESTNUM)
+	{
+		endS = common::Timestamp::now().microSecondsSinceEpoch();
+		printf("Time spend %ld   %ld\n",beginS,endS);
+	}
+	PDEBUG("hMsetCallBack :: ret :: %ld :: %d\n",ret,respond);
 }
 
-void connectCallback(const redisAsyncContext *c, int status) {
-    if (status != REDIS_OK) {
-        printf("Error: %s\n", c->errstr);
-        return;
-    }
-    printf("Connected...\n");
-}
-
-void disconnectCallback(const redisAsyncContext *c, int status) {
-    if (status != REDIS_OK) {
-        printf("Error: %s\n", c->errstr);
-        return;
-    }
-    printf("Disconnected...\n");
-}
-#endif
-
-int main (int argc, char **argv) {
-    signal(SIGPIPE, SIG_IGN);
-#if 0
-    struct event_base *base = event_base_new();
-
-    redisAsyncContext *c = redisAsyncConnect("127.0.0.1", 6379);
-    if (c->err) {
-        /* Let *c leak for now... */
-        printf("Error: %s\n", c->errstr);
-        return 1;
-    }
-
-    redisLibeventAttach(c,base);
-    redisAsyncSetConnectCallback(c,connectCallback);
-    redisAsyncSetDisconnectCallback(c,disconnectCallback);
-    redisAsyncCommand(c, NULL, NULL, "SET key %b", argv[argc-1], strlen(argv[argc-1]));
-    redisAsyncCommand(c, getCallback, (char*)"end-1", "GET key");
-    event_base_dispatch(base);
-#endif
-	ASYNCREDIS::RedisAsync hha;
-
+void* RedisInitThread(void* obj)
+{
 	hha.RedisConnect("127.0.0.1", 6379,5);
 	hha.RedisLoop();
+	return NULL;
+}
+
+int main (int argc, char **argv)
+{
+	pthread_t pthreadId_;
+	uint64_t msgId = 4161478067814942313;
+	char redis_key[32] = {0};
+	//char redis_key[32] = "msg:4161478067814942313";
+
+	if (pthread_create(&pthreadId_, NULL, &RedisInitThread, NULL))
+	{
+		printf("RedisInitThread init error \n");
+		return -1;
+	}
+	
+	ASYNCREDIS::HashMap hmsetMap;
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("spno","10655999666456151"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("dnVolume","0"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("tmplparams","15800000000"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("title","15800000000"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("smsgwno","4427"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("userid","iris03"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("ecid","100317"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("exdata","15800000000"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("tmplID","0"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("dldTimes","0"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("dldState","0"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("flag","1"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("upVolume","0"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("smsptcode","PTGW2"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("smsgwno","4427"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("Phone","15800000000"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("genTime","1521447030088"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("volume","0"));
+	hmsetMap.insert(ASYNCREDIS::HashMap::value_type("validtm","24"));
+
+	beginS = common::Timestamp::now().microSecondsSinceEpoch();
+	for(int i = 0;i < TESTNUM;i++)
+	{
+		msgId++;
+		memset(redis_key,0,32);
+		sprintf(redis_key,"msg:%lu",msgId);
+		hha.hmset(redis_key,hmsetMap,std::bind(hMsetCallBack,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),NULL);
+		PDEBUG("hmset :::: %d\n",i);
+	}
+
+	while(1)
+		sleep(60);
+	
     return 0;
 }
 
