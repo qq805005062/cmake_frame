248b0c343458498bd6bd9643606ee676595453ae
zhaoxiaoxiao <zhao_xiaoxiao@foxmail.com>
2019-09-04 17:28:04 +0800

    add module test method

--- src/common/moduleTest.cpp
+++ src/common/moduleTest.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "MD5.h"
+#include "AES.h"
 
 #define PDEBUG(fmt, args...)                    fprintf(stderr, "%s :: %s() %d: DEBUG " fmt " \n", __FILE__, __FUNCTION__, __LINE__, ## args)
 #define PERROR(fmt, args...)                    fprintf(stderr, "%s :: %s() %d: ERROR " fmt " \n", __FILE__, __FUNCTION__, __LINE__, ## args)
@@ -13,6 +14,12 @@ int main(int argc,char **argv)
     PDEBUG("hello world");
     common::stringMd5TohexStr(input, output);
     PDEBUG("output %s::0x%s", input.c_str(), output.c_str());
+
+    std::string aesKey(output.c_str(), 16);
+
+    common::aesEcbEncrypt(input, aesKey, output);
+    common::aesEcbDecrypt(output, aesKey, input);
+    PDEBUG("input %s", input.c_str());
     return 0;
 }
 
